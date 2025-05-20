86f9d5e8dd07758178f958d98884981e202fa9fd
Alexander Skøtt Leismann <asle71355@edu.ucl.dk>
2025-05-20 12:50:41 +0200

    kunde opdateret

--- GettingReal/Model/Kunde.cs
+++ GettingReal/Model/Kunde.cs
@@ -2,7 +2,29 @@
 
 public class Kunde
 {
-	public Kunde()
+	public string Navn { get; private set; }
+	public string Id { get; private set; }
+
+	public double Pris { get; set; }
+
+	public Forbrug forbrug { get; private set; }
+
+
+
+	public Kunde(string navn, string id, double pris, Forbrug forbrug)
 	{
+		Navn = navn;
+		Id = id;
+		Pris = pris;
+		this.forbrug = forbrug;
 	}
+
+	public Kunde GetCostumer()
+	{
+		return this;
+	}
+    public override string ToString()
+    {
+        return $"Name: {Navn} Id: {Id} Price: {Pris}";
+    }
 }
